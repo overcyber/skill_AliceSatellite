6c2653379def1fcb2fcf43c596c1d06a139d6ef7
philipp2310 <41761223+philipp2310@users.noreply.github.com>
2022-06-13 01:34:21 +0200

    device UI click for stop listening added

--- devices/AliceSatellite.py
+++ devices/AliceSatellite.py
@@ -1,5 +1,7 @@
 from core.device.model.Device import Device
 from core.device.model.DeviceAbility import DeviceAbility
+from core.commons import constants
+import json
 
 
 class AliceSatellite(Device):
@@ -16,3 +18,19 @@ class AliceSatellite(Device):
 			'deviceSettings'        : dict(),
 			'abilities'             : [DeviceAbility.PLAY_SOUND, DeviceAbility.CAPTURE_SOUND]
 		}
+
+
+	def onUIClick(self) -> dict:
+		# todo: implement advanced dnd behaviour as well (mute output - but not all?)
+		if self.getParam('micMuted'):
+			# both muted, enable mic
+			self.MqttManager.mqttClient.publish(topic=constants.TOPIC_HOTWORD_TOGGLE_ON,
+												payload=json.dumps({'uid': self.getConfig('uuid')}))
+			self.updateParam('micMuted', False)
+		else:
+			# nothing muted, mute mic
+			self.MqttManager.mqttClient.publish(topic=constants.TOPIC_HOTWORD_TOGGLE_OFF,
+			                                    payload=json.dumps({'uid': self.getConfig('uuid')}))
+			self.updateParam('micMuted', True)
+
+		return super().onUIClick()
